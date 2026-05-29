007f7b40e9d7f6283fc84f29b6738d41396a1d01
alicevoid <amelia.pvc@gmail.com>
2026-05-29 00:45:08 -0700

    getting started with click

--- src/azazel/interface.py
+++ src/azazel/interface.py
@@ -5,3 +5,9 @@ Interface Design
 
 """
 
+import click
+import os 
+
+@click.command()
+@click.option('--port', default=2121, help='Port to listen on (default: 2121)')
+@click.option('--root', default=os.getcwd(), help='Root directory to serve (default: current directory)')
